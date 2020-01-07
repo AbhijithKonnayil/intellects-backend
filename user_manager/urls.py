691aac8d22ee97bc61bc8aced7c8de8939ef5f16
AbhijithKonnayil <iamabhijith.k@gmail.com>
2020-01-07 06:30:38 +0530

    add name to url patterns | add password change view

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -6,18 +6,12 @@ from rest_auth.views import (
 )
 from .views import UserLoginAPIView, UserRegisterAPIView, UserLogoutAPIView
 
-
-
-# urlpatterns = [
-#     url(r'^password/change/$', PasswordChangeView.as_view(),
-#         name='rest_password_change'),
-# ]
-
 urlpatterns = [
-    path('login/', UserLoginAPIView.as_view()),
-    path('register/', UserRegisterAPIView.as_view()),
-    path('logout/', UserLogoutAPIView.as_view()),
+    path('login/', UserLoginAPIView.as_view(),name='user-login'),
+    path('register/', UserRegisterAPIView.as_view(),name='user-register'),
+    path('logout/', UserLogoutAPIView.as_view(),name='user-logout'),
 
+    path('password/change/', PasswordChangeView.as_view(),name='password-change'),
     path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password/reset/confirm/<uidb64>-<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
