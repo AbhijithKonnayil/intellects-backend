56673959d31c5b46951d5aa1a49c1c7598e287ad
AbhijithKonnayil <iamabhijith.k@gmail.com>
2020-01-09 09:30:00 +0530

    models registered in admin

--- content_manager/admin.py
+++ content_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Course, Module, Topic, VideoLecture, VideoComment, QuestionPaper, QuestionSoluntion
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Topic)
+admin.site.register(VideoLecture)
+admin.site.register(VideoComment)
+admin.site.register(QuestionPaper)
+admin.site.register(QuestionSoluntion)
