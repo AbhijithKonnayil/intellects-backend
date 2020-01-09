bc0dca1cd6f577d5c659de2364c47a7bcf213215
AbhijithKonnayil <iamabhijith.k@gmail.com>
2020-01-09 22:11:57 +0530

    add parent to student profile

--- user_manager/models.py
+++ user_manager/models.py
@@ -5,6 +5,7 @@ from django.contrib.postgres.fields import HStoreField
 
 from content_manager.choices import SEMESTER_CHOICES, DEPARTMENT_CHOICES, college_list
 
+
 class User(AbstractUser):
     def phone_validator(value):
         if len(value) != 10:
@@ -13,28 +14,31 @@ class User(AbstractUser):
         if not value.isdigit():
             raise ValidationError(
                 ('Phone number can have only numbers'),
-                )
-    username = models.CharField('phone', max_length=10, unique=True,validators=[phone_validator])
-    #parent = models.ForeignKey('User',null=True,on_delete=models.SET_NULL)
+            )
+    username = models.CharField(
+        'phone', max_length=10, unique=True, validators=[phone_validator])
 
     def __str__(self):
-        return '{}-{}'.format(self.id,self.username)
-    
-    
-class StudentProfile(models.Model):
-    student = models.OneToOneField(User, on_delete=models.CASCADE)
-    semester = semester = models.CharField(max_length=2, choices=SEMESTER_CHOICES,null=True,blank=True)
-    department = models.CharField(max_length=4, choices=DEPARTMENT_CHOICES,null=True,blank=True)
-    register_no = models.CharField(max_length=11,null=True,blank=True)
-    # grades =  models.IntegerField()
-
-    def __str__(self):
-        return '{}'.format(self.student.username)
+        return '{}-{}'.format(self.id, self.username)
 
 
 class ParentProfile(models.Model):
     parent = models.OneToOneField(User, on_delete=models.CASCADE)
-    
 
     def __str__(self):
         return '{}'.format(self.parent.username)
+
+
+class StudentProfile(models.Model):
+    student = models.OneToOneField(User, on_delete=models.CASCADE)
+    semester = semester = models.CharField(
+        max_length=2, choices=SEMESTER_CHOICES, null=True, blank=True)
+    department = models.CharField(
+        max_length=4, choices=DEPARTMENT_CHOICES, null=True, blank=True)
+    register_no = models.CharField(max_length=11, null=True, blank=True)
+    parent = models.ForeignKey(
+        ParentProfile, null=True, on_delete=models.SET_NULL)
+    # grades =  models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.student.username)
